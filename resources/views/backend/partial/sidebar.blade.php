4204d84fe6bda4ae0c7313b193a3b18a116d4bef
AkbarKheun <akbarmajenesmk5@gmail.com>
2023-10-18 08:02:16 +0700

    tambah icon

--- resources/views/backend/partial/sidebar.blade.php
+++ resources/views/backend/partial/sidebar.blade.php
@@ -7,16 +7,54 @@
         </a>
       </li>
       <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+          <i class="ti-pencil-alt menu-icon"></i>
+          <span class="menu-title">Pandaftaran</span>
+        </a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-          <i class="icon-columns menu-icon"></i>
-          <span class="menu-title">Form elements</span>
+          <i class="ti-server menu-icon"></i>
+          <span class="menu-title">Data Induk</span>
           <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="form-elements">
           <ul class="nav flex-column sub-menu">
-            <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
+            <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Data Balita</a></li>
           </ul>
         </div>
+        <div class="collapse" id="form-elements">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Data Ibu Hamil</a></li>
+          </ul>
+        </div>
+        <div class="collapse" id="form-elements">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Data Ibu Nifas</a></li>
+          </ul>
+        </div>
+        <div class="collapse" id="form-elements">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Data KB</a></li>
+          </ul>
+        </div>
+        <div class="collapse" id="form-elements">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Data PUS</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+          <i class="ti-stats-up menu-icon"></i>
+          <span class="menu-title">Statistik Bayi</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+          <i class="ti-gallery menu-icon"></i>
+          <span class="menu-title">Laporan Kegiatan</span>
+        </a>
       </li>
     </ul>
   </nav>
